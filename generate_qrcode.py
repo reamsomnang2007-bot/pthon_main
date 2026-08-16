2af1f6359815b119e1b20ca98539d672936d2eb7
Ream Somnang <reamsomnang2007@gmail.com>
2026-08-16 10:37:12 +0700

    First_Version

---
+++ generate_qrcode.py
@@ -0,0 +1,13 @@
+import qrcode 
+
+url = input("Enter the url: ")
+file_name = input("Enter the file_name: ")
+file_name = f"{file_name}.png"
+
+qr = qrcode.QRCode(border = 5, box_size = 10)
+qr.add_data(url)
+qr.make()
+
+img = qr.make_image(fill_color = "black", back_color = "white")
+img.save(file_name)
+print("Qrcode make successful...!")
